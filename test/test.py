83868d6d946b5af2b04ba1113e101f58315d4add
jeffrey0726 <angulin726@gmail.com>
2019-06-17 23:06:48 +0800

    first UI

---
+++ test/test.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+#from tkinter import *
+from PIL import Image,ImageTk
+import tkinter.font as tkFont
+from tkinter import filedialog
+import os
+
+class Interface:
+    def __init__(self, root):
+        self.root = root
+        root.title("人臉年齡偵測系統")
+        self.ft = tkFont.Font(family='Helvetica', size=30, weight=tkFont.BOLD)
+        self.ftBtn = tkFont.Font(family='Helvetica', size=15, weight=tkFont.BOLD)
+        self.label = tk.Label(self.root,text="歡迎使用年齡偵測系統",font=self.ft,bg='black',fg='SkyBlue1').pack(fill=tk.X)
+        self.img=ImageTk.PhotoImage(file = "C:/Users/user/PycharmProjects/test/helloworld.jpg")
+        self.imgLabel = tk.Label(self.root, image=self.img).pack()
+        self.inBtn = tk.Button(self.root, text="選擇檔案：",font=self.ftBtn, command=self.select_img).pack(fill=tk.X)
+
+    def hideRoot(self):
+        self.root.withdraw()
+
+    def select_img(self):
+        self.tl = tk.Toplevel()
+        #self.tl.geometry("+300+80")
+        self.tl.withdraw()
+        self.file_path = filedialog.askopenfilename(initialdir = "C:/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+        self.rel_path = os.path.relpath(self.file_path)
+        self.tl.update()
+        self.tl.deiconify()
+        self.im=Image.open(self.rel_path)
+        self.im = self.im.resize((224, 224), Image.ANTIALIAS)
+        self.img=ImageTk.PhotoImage(self.im)
+        self.imLabel=tk.Label(self.tl,image=self.img).grid(row=0, column=1)
+        self.hideRoot()
+        self.emptyLabel1 = tk.Label(self.tl, height=10).grid(row=1,column=1)
+        self.closeBtn = tk.Button(self.tl, text="結束程式", font=self.ftBtn, command=self.close_window).grid(row=2, column=0)
+        self.nextBtn = tk.Button(self.tl, text="看下一張", font=self.ftBtn, command=self.close_window).grid(row=2, column=2)
+        self.tl.mainloop()
+
+    def close_window(self):
+        self.tl.destroy()
+        self.root.destroy()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Interface(root)
+    root.mainloop()
