9eab08724e9c426187bcd1c7969a2c8dfdbe596e
stu199712 <stu12233@gmail.com>
2019-06-21 04:35:52 +0800

    version4

--- training.py
+++ training.py
@@ -18,7 +18,7 @@ class ConvolutionalNeuralNetworks():
         # Input shape
         self.img_rows = 224
         self.img_cols = 224
-        self.channels = 3
+        self.channels = 3   #RGB
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
 
         # Configure data loader
@@ -27,9 +27,10 @@ class ConvolutionalNeuralNetworks():
         self.data_loader =DataLoader(train_dataset=self.train_dataset_name,val_dataset=self.val_dataset_name,img_res=(self.img_rows, self.img_cols))
         
         # Build the network
-        optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)#上一版lr=0.002
+        optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)#上一版lr=0.002 (學習率)
         self.CNN_Network = self.build_CNN_Network()
         self.CNN_Network.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        #categorical_crossentropy 用於目標值為分類時
 
         # Input images
         Xtr = Input(shape=self.img_shape)
@@ -45,7 +46,6 @@ class ConvolutionalNeuralNetworks():
                 d = BatchNormalization(momentum=0.8)(d)
             return d
 
-
         def maxpooling2d(layer_input, f_size, stride=2):
             d = MaxPooling2D(pool_size=f_size, strides=stride, padding='valid')(layer_input)
             return d
@@ -56,24 +56,24 @@ class ConvolutionalNeuralNetworks():
 
         def dense(layer_input, f_size, dr=True, lastLayer=True):
             if lastLayer:
-                d = Dense(f_size, activation='softmax')(layer_input)
+                d = Dense(f_size, activation='softmax')(layer_input) #用 Softmax 激活函數到輸出層
             else:
-                d = Dense(f_size, activation='linear')(layer_input)
+                d = Dense(f_size, activation='linear')(layer_input) #建f_size個神經元,隱藏層
                 d = LeakyReLU(alpha=0.2)(d)
                 if dr:
-                    d = Dropout(0.5)(d)
+                    d = Dropout(0.5)(d) #放棄50%神經元
             return d
 
-        # LeNet-5 layers #224*224    6*6
+        # LeNet-5 layers #224 x 224 RGB圖像輸入    
         d0 = Input(shape=self.img_shape)  # Image input
-        d1 = conv2d(d0, filters=6, f_size=5, stride=1, bn=True) #卷積
-        d2 = maxpooling2d(d1, f_size=2, stride=2)   #可能是2x2範圍內取最大值 
-        d3 = conv2d(d2, filters=16, f_size=5, stride=1, bn=True)
-        d4 = maxpooling2d(d3, f_size=2, stride=2)
-        d5 = flatten(d4)
-        d6 = dense(d5, f_size=120, dr=True, lastLayer=False)
-        d7 = dense(d6, f_size=84, dr=True, lastLayer=False)
-        d8 = dense(d7, f_size=7, dr=False, lastLayer=True) #由於有7種class 所以改成7
+        d1 = conv2d(d0, filters=6, f_size=5, stride=1, bn=True) # 5 x 5 x 1 x 6 + 6 = 156   卷積成220 x 220 x 6
+        d2 = maxpooling2d(d1, f_size=2, stride=2)   #2x2 filters範圍內取最大值                過濾成110 x 110 x 6
+        d3 = conv2d(d2, filters=16, f_size=5, stride=1, bn=True) # 5 x 5 x 1 x 10 + 16 =  1516    106 x 106 x 16
+        d4 = maxpooling2d(d3, f_size=2, stride=2)   #2x2 filters範圍內取最大值                      53 x 53  x 16
+        d5 = flatten(d4)                                        #44944
+        d6 = dense(d5, f_size=120, dr=True, lastLayer=False)    #120
+        d7 = dense(d6, f_size=84, dr=True, lastLayer=False)     #84
+        d8 = dense(d7, f_size=7, dr=False, lastLayer=True) #由於有7種class 所以改成7,一維向量 輸出層
         return Model(d0, d8)
 
     def train(self, epochs, batch_size=1, sample_interval=50):
@@ -96,7 +96,7 @@ class ConvolutionalNeuralNetworks():
                 print(batch_i)
 
                 # If at save interval => do validation and save model
-                if (batch_i + 1) % sample_interval == 49:
+                if (batch_i + 1) % sample_interval == 149:
                     self.validation(epoch)
 
     def validation(self, epoch):
@@ -110,4 +110,4 @@ class ConvolutionalNeuralNetworks():
 if __name__ == '__main__':
     #     '''training model'''
     my_CNN_Model = ConvolutionalNeuralNetworks()
-    my_CNN_Model.train(epochs=10, batch_size=20, sample_interval=1266)
+    my_CNN_Model.train(epochs=2, batch_size=20, sample_interval=1266)
